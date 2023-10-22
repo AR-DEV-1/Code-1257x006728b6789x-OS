35edce5a30d101c26b3732c6bcfbd202e6dac25e
AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>
2023-10-22 20:40:24 +0500

    Update kernal.cpp

--- kernal.cpp
+++ kernal.cpp
@@ -1,3 +1,23 @@
 // kernal 
 // written in C++
 // bootloader was written in C
+
+
+// kernel.cpp
+extern "C" {
+    void kernel_main() {
+        // Kernel code -->
+        const char* kernelbasic = "Kernel Is Still in development!";
+        volatile unsigned short* video_memory = (volatile unsigned short*)0xB8000;
+
+        for (int i = 0; hello[i] != '\0'; i++) {
+            video_memory[i] = (video_memory[i] & 0xFF00) | kernelbasic[i];
+        }
+    }
+}
+
+extern "C" void _start() {
+    kernel_main();
+
+    asm volatile ("hlt");
+}
